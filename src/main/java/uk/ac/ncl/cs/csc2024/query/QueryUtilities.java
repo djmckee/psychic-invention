a7a08f312ff5a63fdc2bb320e4c7653538917853
Dylan McKee <dylan@djmckee.co.uk>
2016-05-02 14:10:33 +0100

    Documentation

--- src/main/java/uk/ac/ncl/cs/csc2024/query/QueryUtilities.java
+++ src/main/java/uk/ac/ncl/cs/csc2024/query/QueryUtilities.java
@@ -8,13 +8,38 @@ import uk.ac.ncl.cs.csc2024.operator.Operator;
 import java.util.List;
 
 /**
- * Created by djmckee on 16/04/2016.
+ * Convenience methods to find operator by name and find a bus stop by ID number.
+ * I created this class to reduce code and logic duplication, and ensure that the
+ * 'Queries' classes to not become cluttered with these accessor methods.
+ *
+ * Created by Dylan McKee on 16/04/2016.
+ * @author Dylan McKee
  */
 public class QueryUtilities {
 
+    /**
+     * The HQL Query to find a bus stop by name, to be used in and have the 'name' variable placeholder populated
+     * by the findOperatorByName method.
+     */
+    private static final String FIND_OPERATOR_BY_NAME_HQL_QUERY = "select o from Operator o where o.name=:name";
+
+    /**
+     * The HQL Query to find a bus stop by name, to be used in and have the 'id' variable placeholder populated
+     * by the findBusStopWithID method.
+     */
+    private static final String FIND_BUS_STOP_BY_ID_HQL_QUERY = "select s from BusStop s where s.id=:id";
+
+    /**
+     * Finds the Operator instance from the operators entirety table that matches the name passed into this method.
+     *
+     * @param session the Hibernate session to run the query within.
+     * @param name the name of the Operator instance to search for.
+     * @return the Operator instance that matches the name passed to this method; or null if no matching operator
+     * could be found.
+     */
     public static Operator findOperatorByName(Session session, String name) {
         // Look for the operator with the name we've been passed...
-        Query operatorQuery = session.createQuery("select o from Operator o where o.name=:name");
+        Query operatorQuery = session.createQuery(FIND_OPERATOR_BY_NAME_HQL_QUERY);
         operatorQuery.setString("name", name);
 
         List<Operator> operatorResults = (List<Operator>) operatorQuery.list();
@@ -34,8 +59,15 @@ public class QueryUtilities {
 
     }
 
+    /**
+     * Finds and returns the BusStop instance with the id that matches the id number passed into this method.
+     *
+     * @param session the Hibernate session to run this query within.
+     * @param id the id of the Bus Stop instance to search for and return (iff it exists)
+     * @return the Bus Stop instance that matches the ID being searched for, if one exists. Otherwise null.
+     */
     public static BusStop findBusStopWithID(Session session, int id) {
-        Query sessionQuery = session.createQuery("select s from BusStop s where s.id=:id");
+        Query sessionQuery = session.createQuery(FIND_BUS_STOP_BY_ID_HQL_QUERY);
         sessionQuery.setInteger("id", id);
 
         List<BusStop> stops= (List<BusStop>) sessionQuery.list();
